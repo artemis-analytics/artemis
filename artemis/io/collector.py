8a637998208be541e2ef2553b83307f6a3ab4206
Ryan White <ryan.white@cloud.statcan.ca>
2019-04-04 21:38:07 +0000

    Add Collector test cases

--- artemis/io/collector.py
+++ artemis/io/collector.py
@@ -20,7 +20,6 @@ from artemis.core.algo import AlgoBase
 from artemis.core.tree import Tree
 from artemis.core.datastore import ArrowSets
 from artemis.core.tool import ToolStore
-from artemis.core.properties import JobProperties
 from google.protobuf import text_format
 
 
@@ -33,14 +32,6 @@ class Collector(AlgoBase):
 
     def __init__(self, name, **kwargs):
         '''
-        Access the Base logger directly through
-        self.__logger
-        Derived class use the classmethods for info, debug, warn, error
-        All formatting, loglevel checks, etc...
-        can be done through the classmethods
-
-        Can we use staticmethods in artemis to make uniform
-        formatting of info, debug, warn, error?
         '''
         options = dict(CollectorOptions())
         options.update(kwargs)
@@ -55,14 +46,11 @@ class Collector(AlgoBase):
         self.job_id = self.properties.job_id
         self.path = self.properties.path
 
-        self._jp = None
         self.tools = None
-        self.hbook = None
         self.tree = None
 
     def initialize(self):
         self.__logger.info("initialize")
-        self._jp = JobProperties()
         self.tools = ToolStore()
         self.tree = Tree()
 
@@ -79,31 +67,35 @@ class Collector(AlgoBase):
             if toolcfg.name == "bufferwriter":
                 _wrtcfg = toolcfg
 
-        try:
-            for leaf in self.tree.leaves:
-                self.__logger.info("Leave node %s", leaf)
-                node = self.tree.get_node_by_key(leaf)
-                key = node.key
-                try:
-                    _last = node.payload[-1].get_data()
-                except IndexError:
-                    self.__logger.error("Cannot retrieve payload! %s", key)
-                    raise
+        if _wrtcfg is None:
+            self.__logger.error("BufferWriter not configured")
+            raise KeyError
 
-                # TODO
-                # Properly configure the properties in the job config
-                # This is a workaround which overwrites any set properties
-                if isinstance(_last, pa.lib.RecordBatch):
+        for leaf in self.tree.leaves:
+            self.__logger.info("Leave node %s", leaf)
+            node = self.tree.get_node_by_key(leaf)
+            key = node.key
+            try:
+                _last = node.payload[-1].get_data()
+            except IndexError:
+                self.__logger.error("Cannot retrieve payload! %s", key)
+                raise
+
+            # TODO
+            # Properly configure the properties in the job config
+            # This is a workaround which overwrites any set properties
+            if isinstance(_last, pa.lib.RecordBatch):
+                try:
                     _wrtcfg.name = "writer_" + key
-                    self.__logger.info("Add Tool %s", _wrtcfg.name)
-                    self.tools.add(self.__logger, _wrtcfg)
-                    self.tools.get(_wrtcfg.name)._schema = _last.schema
-                    self.tools.get(_wrtcfg.name)._fbasename = self.job_id
-                    self.tools.get(_wrtcfg.name)._path = self.path
-                    self.tools.get(_wrtcfg.name).initialize()
-        except Exception:
-            self.__logger.error("Problem creating output streams")
-            raise
+                except AttributeError:
+                    self.__logger.error("BufferWriter misconfigured")
+                    raise
+                self.__logger.info("Add Tool %s", _wrtcfg.name)
+                self.tools.add(self.__logger, _wrtcfg)
+                self.tools.get(_wrtcfg.name)._schema = _last.schema
+                self.tools.get(_wrtcfg.name)._fbasename = self.job_id
+                self.tools.get(_wrtcfg.name)._path = self.path
+                self.tools.get(_wrtcfg.name).initialize()
 
         # Batches serialized, clear the tree
         try:
@@ -130,8 +122,17 @@ class Collector(AlgoBase):
             self.__logger.info("COLLECT: Total memory reached")
             try:
                 result_, time_ = self._collect()
+            except IndexError:
+                self.__logger.error("Error with leaf payload")
+                raise
+            except ValueError:
+                self.__logger.error("Error with batch schema")
+                raise
+            except IOError:
+                self.__logger.error("IOError in writer")
+                raise
             except Exception:
-                self.__logger.error("Problem collecting")
+                self.__logger.error("Unknown error")
                 raise
             self.__logger.info("Allocated %i", pa.total_allocated_bytes())
             self.hbook.fill('artemis', 'time.collect', time_)
@@ -155,26 +156,39 @@ class Collector(AlgoBase):
             els = node.payload
             self.__logger.info('Batches of leaf %s', len(els))
             _name = "writer_"+node.key
-            if isinstance(els[-1].get_data(), pa.lib.RecordBatch):
+            _last = None
+            try:
+                _last = els[-1].get_data()
+            except IndexError:
+                self.__logger.error("%s payload empty", leaf)
+                raise
+            except Exception:
+                self.__logger.error("%s unknown error", leaf)
+                raise
+            if isinstance(_last, pa.lib.RecordBatch):
                 self.__logger.info("RecordBatch")
                 self.__logger.info("Allocated %i", pa.total_allocated_bytes())
                 _schema_batch = els[-1].get_data().schema
-                # TODO
-                # Get the pyarrow schema as early as possible
-                # Store/retrieive from the metastore
-                # Do not assume each file has same schema!!!!
+
+                if self.tools.get(_name)._schema != _schema_batch:
+                    self.__logger.error("Schema mismatch")
+                    self.__logger.error("Writer schema %s",
+                                        self.tools.get(_name)._schema)
+                    self.__logger.error("Batch schema %s", _schema_batch)
+                    raise ValueError
                 try:
-                    self.tools.get("writer_"+node.key)._schema = \
-                        _schema_batch
                     self.tools.get("writer_"+node.key).write(els)
                     self.__logger.info("Records %i Batches %i",
                                        self.tools.get(_name)._nrecords,
                                        self.tools.get(_name)._nbatches)
+                except IOError:
+                    self.__logger.error("IOError in buffer writer")
+                    raise
                 except Exception:
-                    self.__logger.error("Error in buffer writer")
+                    self.__logger.error("Unknown error in buffer writer")
                     raise
             else:
-                self.__logger.info("%s", type(els[-1].get_data()))
+                self.__logger.debug("%s", type(els[-1].get_data()))
 
         # Batches serialized, clear the tree to flush memory
         try:
