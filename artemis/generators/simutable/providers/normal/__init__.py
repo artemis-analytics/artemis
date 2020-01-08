3d27747e86fd4dd3a8cfb3da3f7b65fad8707742
terminal <rgill0030@gmail.com>
2020-01-08 14:38:11 -0500

    Added untested Normal Provider

--- artemis/generators/simutable/providers/normal/__init__.py
+++ artemis/generators/simutable/providers/normal/__init__.py
@@ -15,15 +15,37 @@ from faker.providers import BaseProvider
 
 
 class Provider(BaseProvider):
-    def normal(self):
-        mu = 0
-        sigma = 1
-
+    def normal(self, params):
+        mu, sigma = check_double(params, normal_dist.__name__)
         return self.generator.random.normalvariate(mu, sigma)
 
+class TemporaryErrorClass():
+    def check_double(self, params, name):
+        try:
+            # Verify that the param is, in fact, a list
+            assert isinstance(params, list)
+        except AssertionError:
+            raise TypeError("Illegal parameter type in %s", name)
+        try:
+            # Verify that the length is only 2
+            assert len(params) == 2
+        except AssertionError:
+            # Complain if it is anything else
+            if len(params) > 2:
+                raise ValueError("Illegal parameters passed in function %s", name)
+            else:
+                raise ValueError("Insufficient parameters passed in function %s", name)
+        try:
+            # Verify you can unpack into X and Y as floats
+            x, y = [float(x) for x in params]
+            # Return those values
+            return [x, y]
+        # Complain if the values can't be formatted into floats
+        except TypeError:
+            raise TypeError("Illegal types in parameter array in function %s", name)
 
 class TestCase(unittest.TestCase):
-    def test(self):
+    def test(self, params):
         fake = Faker()
 
         provider = Provider(fake)
