a56e47f248f79d69a91888e8b016f34522c08247
Ryan White <ryan.white4@canada.ca>
2020-01-24 18:43:29 +0000

    add the sphinx rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import sphinx_rtd_theme
 #import os
 #import sys
 #sys.path.insert(0, os.path.abspath('.'))
@@ -27,7 +28,7 @@ author = 'Ryan M. White, Dominic Parent, Russell Gill'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.autosectionlabel','sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.autosectionlabel','sphinx.ext.autosummary','sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -43,12 +44,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 autosectionlabel_prefix_document = True
 
