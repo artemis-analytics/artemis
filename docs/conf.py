9828ca563101e3b469ed8fc2d95cb18884572e66
Dominic Parent <dominic.parent@canada.ca>
2020-03-25 08:22:06 -0400

    Test import path config.

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,9 @@
 import sphinx_rtd_theme
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
-#sys.path.extend([os.path.join(os.path.dirname(__file__),'..','../..')])
+#sys.path.insert(0, os.path.abspath('.'))
+#sys.path.insert(0, os.path.abspath('../'))
+sys.path.extend([os.path.join(os.path.dirname(__file__),'..','../..')])
 
 # -- Project information -----------------------------------------------------
 
