d3312e76e4f8eaa28eb296e64c57f030fa9de3e4
Ryan White <ryanmwhitephd@gmail.com>
2020-03-30 07:26:31 -0400

    add cython to docs reqs

--- setup.py
+++ setup.py
@@ -16,37 +16,48 @@ from setuptools import Extension, setup, find_packages
 from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
 
-import numpy as np
 import os
-import pyarrow as pa
+
 
 def main():
     # Define all Cython extensions here - by default, link to arrow and disable warnings
-    make_ext('artemis.tools._filtercoltool', sources='./artemis/tools/_filtercoltool.pyx')
-    make_ext('artemis.externals.accumulation_tree.accumulation_tree', sources='./artemis/externals/accumulation_tree/accumulation_tree.pyx')
+    make_ext(
+        "artemis.tools._filtercoltool", sources="./artemis/tools/_filtercoltool.pyx"
+    )
+    make_ext(
+        "artemis.externals.accumulation_tree.accumulation_tree",
+        sources="./artemis/externals/accumulation_tree/accumulation_tree.pyx",
+    )
     # Any non-Cython extensions should be manually added to 'ext_modules'.
 
-    setup(name='artemis',
-        version='0.5.0',
-        author='Ryan White',
-        author_email='ryan.white4@canada.ca',
+    setup(
+        name="artemis",
+        version="0.5.0",
+        author="Ryan White",
+        author_email="ryan.white4@canada.ca",
         packages=find_packages(),
         ext_modules=cythonize(ext_modules),
-        cmdclass={'build_ext': build_ext_},
-        package_data={
-            '':['*.so', '*.pyx', '*.pxd', '*.h']},
+        cmdclass={"build_ext": build_ext_},
+        package_data={"": ["*.so", "*.pyx", "*.pxd", "*.h"]},
         install_requires=[],
-        description="Stateful processing framework for administrative data powered by Apache Arrow."
+        description="Stateful processing framework for administrative data powered by Apache Arrow.",
     )
+
 
 def link_arrow(ext):
     """
     Link the extension to the Arrow C++ library.
     """
-    ext.include_dirs.append(np.get_include())  # Arrow is working on removing this dependency
+    import numpy as np
+    import pyarrow as pa
+
+    ext.include_dirs.append(
+        np.get_include()
+    )  # Arrow is working on removing this dependency
     ext.include_dirs.append(pa.get_include())
     ext.library_dirs.extend(pa.get_library_dirs())
     ext.libraries.extend(pa.get_libraries())
+
 
 def make_ext(name, sources, include_arrow=True, quiet=True):
     """
@@ -72,17 +83,18 @@ def make_ext(name, sources, include_arrow=True, quiet=True):
         The extension has already been added to the build list, but you can still add
         more options (ex. ext.include_dirs.append('NEW_INCLUDE_DIRS')).
     """
+
     if not isinstance(sources, list):
         sources = [sources]
     ext = Extension(name=name, sources=sources)
-    
+
     if quiet:
-        ext.extra_compile_args.append('-w')
+        ext.extra_compile_args.append("-w")
     if include_arrow:
         link_arrow(ext)
-    if os.name == 'posix':
-        ext.extra_compile_args.append('-std=c++11')
-    
+    if os.name == "posix":
+        ext.extra_compile_args.append("-std=c++11")
+
     ext_modules.append(ext)
     return ext
 
@@ -95,11 +107,13 @@ class build_ext_(build_ext):
     compiler. This compiler flag is not supported in C++. The compiler will ignore it, but
     we can remove the warning by manually removing the flag.
     """
+
     def build_extensions(self):
-        if '-Wstrict-prototypes' in self.compiler.compiler_so:
-            self.compiler.compiler_so.remove('-Wstrict-prototypes')
+        if "-Wstrict-prototypes" in self.compiler.compiler_so:
+            self.compiler.compiler_so.remove("-Wstrict-prototypes")
         super().build_extensions()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     ext_modules = []  # extensions are passed here by 'make_ext'
     main()
