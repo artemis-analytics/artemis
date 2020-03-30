888c91c66f8ae747406d268bdfc02a9e4671b4f5
Ryan White <ryanmwhitephd@gmail.com>
2020-03-30 07:47:49 -0400

    update sphinx path to source

--- docs/conf.py
+++ docs/conf.py
@@ -15,37 +15,40 @@ import os
 import sys
 
 # Minimum version, enforced by sphinx
-needs_sphinx = '2.4.4'
+needs_sphinx = "2.4.4"
 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
-#sys.path.extend([os.path.join(os.path.dirname(__file__),'..','../..')])
+# sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath("../artemis"))
+# sys.path.extend([os.path.join(os.path.dirname(__file__),'..','../..')])
 
 # -- Project information -----------------------------------------------------
 
-project = 'pyartemis'
-copyright = '2020, Ryan M. White, Dominic Parent, Russell Gill'
-author = 'Ryan M. White, Dominic Parent, Russell Gill'
+project = "pyartemis"
+copyright = "2020, Ryan M. White, Dominic Parent, Russell Gill"
+author = "Ryan M. White, Dominic Parent, Russell Gill"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.autosectionlabel',
-              'sphinx.ext.autosummary',
-              'sphinx_rtd_theme',
-              'sphinx.ext.napoleon']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autosummary",
+    "sphinx_rtd_theme",
+    "sphinx.ext.napoleon",
+]
 #              'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,18 +56,20 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 autosectionlabel_prefix_document = True
 
-source_suffix = ['.rst']
-autodoc_default_options={'members': True, 
-                         'member-order': 'groupwise', 
-                         'inherited-members': True,
-                         'show-inheritance':True}
+source_suffix = [".rst"]
+autodoc_default_options = {
+    "members": True,
+    "member-order": "groupwise",
+    "inherited-members": True,
+    "show-inheritance": True,
+}
 autosummary_generate = True
