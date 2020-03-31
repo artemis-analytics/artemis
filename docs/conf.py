096b3352734eb05e5ddd716a5d5d16cbfc7cba90
Ryan White <ryanmwhitephd@gmail.com>
2020-03-31 15:15:23 -0400

    use autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -10,17 +10,19 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sphinx_rtd_theme
+import sphinx_rtd_theme  # noqa: F401
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-#sys.path.extend([os.path.join(os.path.dirname(__file__),'..','../..')])
+
+needs_sphinx = "2.4.4"
+
+sys.path.extend([os.path.join(os.path.dirname(__file__), "..", "../..")])
 
 # -- Project information -----------------------------------------------------
 
-project = 'pyartemis'
-copyright = '2020, Ryan M. White, Dominic Parent, Russell Gill'
-author = 'Ryan M. White, Dominic Parent, Russell Gill'
+project = "pyartemis"
+copyright = "2020, Ryan M. White, Dominic Parent, Russell Gill"
+author = "Ryan M. White, Dominic Parent, Russell Gill"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,20 +30,23 @@ author = 'Ryan M. White, Dominic Parent, Russell Gill'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.autosectionlabel',
-              'sphinx.ext.autosummary',
-              'sphinx_rtd_theme',
-              'sphinx.ext.napoleon']
+extensions = [
+    "sphinx.ext.autodoc",
+    "autoapi.extension",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autosummary",
+    "sphinx_rtd_theme",
+    "sphinx.ext.napoleon",
+]
 #              'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,18 +54,21 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 autosectionlabel_prefix_document = True
 
-source_suffix = ['.rst']
-autodoc_default_options={'members': True, 
-                         'member-order': 'groupwise', 
-                         'inherited-members': True,
-                         'show-inheritance':True}
+source_suffix = [".rst"]
+autodoc_default_options = {
+    "members": True,
+    "member-order": "groupwise",
+    "inherited-members": True,
+    "show-inheritance": True,
+}
 autosummary_generate = True
+autoapi_dirs = ["../artemis"]
